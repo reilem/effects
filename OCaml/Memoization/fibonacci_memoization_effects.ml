183dbf0d74d3bd294b3bc78d407537f0ad3b7269
Reinert <reilemx@gmail.com>
2018-02-14 15:14:34 +0100

    Implemented fibonacci with memoization

---
+++ OCaml/Memoization/fibonacci_memoization_effects.ml
@@ -0,0 +1,25 @@
+open Hashtbl
+
+effect Put : (int * int) -> unit
+
+effect Get : int -> int option
+
+let rec fibo_mem = function
+  | 0 -> 1
+  | 1 -> 1
+  | n -> match perform (Get n) with
+    | Some x -> x
+    | None -> let value = fibo_mem (n - 1) + fibo_mem (n - 2) in
+        perform (Put (n,value)); value
+
+let fibo n =
+  let store = Hashtbl.create 100 in
+    try fibo_mem n with
+    | effect (Put (a,b)) k ->
+      Hashtbl.add store a b;
+      continue k ()
+    | effect (Get n)     k ->
+      if Hashtbl.mem store n then
+        continue k (Some (Hashtbl.find store n))
+      else
+        continue k None
--- OCaml/TreeAlgorithm/ideas.md
+++ OCaml/TreeAlgorithm/ideas.md
@@ -1,9 +1,15 @@
 # Tree Algorithms
 
-1. Mini-max with pruning
-2. Balanced Tree - all child nodes have equal amount of leaves
-3. Depth-first backtracking
+1. Fringe
+2. Mini-max with pruning
+3. Balanced Tree - all child nodes have equal amount of leaves
+4. Depth-first backtracking
 
 ## Balanced Tree
 
-1. 
+1. Each node has list of children
+2. Calc length of children and compare that they are equal
+
+## Fringe
+
+http://nsl.com/papers/samefringe.htm
