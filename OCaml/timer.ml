8bf7381c3d9ab2934576313a9307e9c18cdc15f3
Lucas <it.lucasengels@gmail.com>
2018-02-14 19:14:14 +0100

    v2.0

--- OCaml/timer.ml
+++ OCaml/timer.ml
@@ -1,3 +1,7 @@
+open Random
+open Printf
+open List
+
 let timer f x =
     let t0 = Sys.time()                                         
     in let () = f x                                            
@@ -11,11 +15,33 @@ let run f n =
   | None -> print_endline "No solutions were found"
   | Some l -> print_endline "A solution was found"
 
+  
+let rec bitstring_generator n =
+    match n with
+    | 0 -> []
+    | n -> Random.int 2 :: (bitstring_generator (n-1))
+
       
 let main () =
-     let oc = open_out "test.txt" in
-     fprintf oc "%f\n" (timer run "INSERT FUNCTION HERE" 8);
-     close_out oc
 
+    List.iter print_int (bitstring_generator 9)
+    
+(*
+     let oc = open_out "test.txt" in
+     fprintf oc "%f\n" (timer queens_generator "INSERT FUNCTION HERE" 8);
+     close_out oc
+*) 
+     
+     
+     (*
+let queens_generator upperlimit =
+    for n = 1 to upperlimit do
+        run "FUNCTION" n
+    done
+    *)
+    
+
+    
      
 let () = main ()
+
