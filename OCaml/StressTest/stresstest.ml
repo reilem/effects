b81648016fab32001eb515e03b7b156fc4b0f60d
Reinert <reilemx@gmail.com>
2018-02-20 16:29:54 +0100

    Stress test cleanup

--- OCaml/StressTest/stresstest.ml
+++ OCaml/StressTest/stresstest.ml
@@ -1,22 +1,20 @@
 open Printf
 open Sys
 
-effect E : unit -> bool
+effect E : bool
 
 module Loop : sig
   val solve : int -> unit
 end =
 struct
   let rec loop () =
-    if perform (E ()) then
-      loop ()
-    else
-      ()
+    if perform E then loop ()
+    else ()
 
   let solve n =
     let count = ref 0 in
     try loop () with
-    | effect (E a) k ->
+    | effect E k ->
       count := !count + 1;
       if !count < n then
         continue k true
