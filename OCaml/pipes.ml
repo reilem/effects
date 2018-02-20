67336ac3122f06908a96d98ff5f09ae3c48d8668
Reinert <reilemx@gmail.com>
2018-02-20 14:08:16 +0100

    Force move pipes + stresstest. Improve pipes implementation

--- OCaml/pipes.ml
+++ OCaml/pipes.ml
@@ -1,31 +1,23 @@
 open List
-open Printf
-
-effect Check : int list -> int list
-
-let negate = function | 0 -> -1 | n -> n
 
 module Pipes : sig
   val solve : int list -> int list
 end =
 struct
+  effect Check : int list -> unit
+
+  let negate = function | 0 -> -1 | n -> n
+
   let rec find_subarray lst =
-    let rec find_sub best = function
-      | [] -> best
-      | ary ->
-        let current_best = perform (Check ary) in
-        let next_best = find_sub best (tl ary) in
-        if (length current_best) > (length next_best) then
-          rev current_best
-        else
-          next_best
-    in find_sub [] lst
+  match lst with
+  | []  -> []
+  | ary -> perform (Check ary); find_subarray @@ tl ary
 
   let solve (lst : int list) =
     try find_subarray lst with
     | effect (Check sublst) k ->
       let rec sub_search sum acc best = function
-        | [] -> best
+        | [] -> rev best
         | next::rest ->
           let newsum = sum + (negate next) in
           let newacc  = next::acc in
@@ -35,5 +27,9 @@ struct
             sub_search newsum newacc best rest
       in
       let current_best = sub_search 0 [] [] sublst in
-      continue k current_best
+      let next_best = continue k () in
+      if length current_best > length next_best then
+        current_best
+      else
+        next_best
 end
