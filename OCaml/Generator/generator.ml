8dd4893dfd42b6d902eb2753dcd58c24ab1b5a70
Reinert <reilemx@gmail.com>
2018-02-21 13:41:01 +0100

    Cleaned code and added to .gitignore

--- OCaml/Generator/generator.ml
+++ OCaml/Generator/generator.ml
@@ -17,9 +17,7 @@ let rec BS n =
     match n with
     | 0 -> []
     | _ -> Random.int 2 :: (BS (n-1))
-    
 
-    
 let randomExp case =
     let bit1 = Random.int 2 in
     let bit2 = Random.int 2 in
@@ -27,14 +25,12 @@ let randomExp case =
     | 0 -> ("AND " ^ (string_of_int bit1)) ^ (string_of_int bit2)
     | 1 -> ("OR " ^ (string_of_int bit1)) ^ (string_of_int bit2)
     | 2 -> "NOT " ^ (string_of_int bit1)
-    
+
 let rec PRS n =
     match n with
     | 0 -> ""
     | _ -> (randomExp (Random.int 3)) ^ (BS (n-1))
-    
+
 
 
 end
-
-
