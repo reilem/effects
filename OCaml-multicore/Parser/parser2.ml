a26c3d541f5d05c48e23c71d7d95642d93ee1770
Reinert <reilemx@gmail.com>
2018-04-06 12:34:50 +0200

    One plus parser

--- OCaml-multicore/Parser/parser2.ml
+++ OCaml-multicore/Parser/parser2.ml
@@ -1,30 +1,29 @@
+open Printf
 
 effect One  : unit
-effect Plus : bool
+effect Plus : unit
 
-let str = "+ 1 + 2 1"
+let str = "1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1"
 
 let explode s =
   let rec expl i l =
-    if i < 0 then l else
-      let chr = s.[i] in
-        if chr == ' ' then expl (i - 1) l else expl (i - 1) (s.[i] :: l)
-     in
+    if i < 0 then l else expl (i - 1) (s.[i] :: l) in
   expl (String.length s - 1) []
 
 let rec program = function
-  | '1'::xs       -> 1
-  | '2'::xs       -> 2
-  | '3'::xs       -> 3
-  | '+'::(x::xs)  -> if perform Plus then program (x::xs) else program xs
+  | '1'::xs       -> perform One; program xs
+  | '+'::xs       -> perform Plus; program xs
   | _::xs         -> program xs
   | []            -> 0
 
-
 let run =
   match program @@ explode str with
-  | effect Plus k   -> (continue (Obj.clone_continuation k) true) + (continue (Obj.clone_continuation k) false)
-  | x               -> x
+  | effect One  k   -> (fun s -> continue k () (1::s))
+  | effect Plus k   -> (fun s -> match s with
+    | [x]   -> (match continue k () [] with | [y] -> [(x + y)] | _ -> [])
+    | _      -> [])
+  | x               -> (fun s -> s)
 
+let _ = run []
 
 (* #use "parser2.ml";; *)
