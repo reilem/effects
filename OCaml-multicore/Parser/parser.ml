34db42239fe00af12a755aad110bd085ae1a3dfa
Reinert <reilemx@gmail.com>
2018-04-08 16:12:56 +0200

    Right to left bool parser with implicit state

--- OCaml-multicore/Parser/parser.ml
+++ OCaml-multicore/Parser/parser.ml
@@ -1,52 +1,19 @@
-open List
 open Printf
 
 module Parser : sig
-  (* Takes a file (string) and parses its contents *)
-  val parse : string -> bool
   (* Takes a string and parses it *)
-  val solve : string -> bool
+  val solve : string -> bool list
 end =
 struct
-  exception Lexical_Error
-  exception Syntax_Error
+  exception Unrecognized_Symbol
 
-  type expression = TRUE | FALSE
-    | AND of expression * expression
-    | OR of expression * expression
-    | NOT of expression
-
-  let rec print_expr = function
-    | TRUE            -> printf "TRUE"
-    | FALSE           -> printf "FALSE"
-    | NOT exp         ->
-      printf "NOT("; print_expr exp; printf ")"
-    | AND (exp1,exp2) ->
-      printf "AND("; print_expr exp1; printf ","; print_expr exp2; printf ")"
-    | OR (exp1,exp2) ->
-      printf "OR("; print_expr exp1; printf ","; print_expr exp2; printf ")"
-
-  type token =
-    | TRUE_T
-    | FALSE_T
-    | AND_T
-    | OR_T
-    | NOT_T
-    | LEFT_T
-    | RIGHT_T
-
-  let print_token = function
-    | TRUE_T  -> printf "TRUE;"
-    | FALSE_T -> printf "FALSE;"
-    | AND_T   -> printf "AND;"
-    | OR_T    -> printf "OR;"
-    | NOT_T   -> printf "NOT;"
-    | LEFT_T  -> printf "LEFT_BRACK;"
-    | RIGHT_T -> printf "RIGHT_BRACK;"
-
-  let rec print_token_list = function
-    | []      -> printf "\n"
-    | a::rest -> print_token a; print_token_list rest
+  effect True : unit
+  effect False : unit
+  effect And: unit
+  effect Or: unit
+  effect Not: unit
+  effect L_Br: unit
+  effect R_Br: unit
 
   let explode s =
     let rec expl i l =
@@ -54,77 +21,48 @@ struct
       expl (i - 1) (s.[i] :: l) in
     expl (String.length s - 1) []
 
-  let rec find_next_exp_list (acc : token list) (lst : token list) (depth : int) : (token list * token list) =
-    match lst with
-    | []            -> (rev acc,[])
-    | LEFT_T::rest  ->
-      if depth == 0 then
-        find_next_exp_list acc rest 1
-      else
-        find_next_exp_list (LEFT_T::acc) rest (depth + 1)
-    | RIGHT_T::rest ->
-      if depth == 1 then
-        (rev acc,rest)
-      else
-        find_next_exp_list (RIGHT_T::acc) rest (depth - 1)
-    | any::rest     ->
-      if depth == 0 then
-        ([any],rest)
-      else
-        find_next_exp_list (any::acc) rest depth
-
-  let rec evaluate (expr : expression) : bool =
-    match expr with
-    | TRUE            -> true
-    | FALSE           -> false
-    | AND (exp1,exp2) -> evaluate exp1 && evaluate exp2
-    | OR  (exp1,exp2) -> evaluate exp1 || evaluate exp2
-    | NOT exp         -> not @@ evaluate exp
-
-  let rec analyse (lst : token list) : expression =
-    match lst with
-    | AND_T::rest   ->
-      let (exp_left,remainder) = find_next_exp_list [] rest 0 in
-      let (exp_right,_) = find_next_exp_list [] remainder 0 in
-      AND (analyse exp_left,analyse exp_right)
-    | OR_T::rest   ->
-      let (exp_left,remainder) = find_next_exp_list [] rest 0 in
-      let (exp_right,_) = find_next_exp_list [] remainder 0 in
-      OR (analyse exp_left,analyse exp_right)
-    | NOT_T::rest   ->
-      let (exp,_) = find_next_exp_list [] rest 0 in
-      NOT (analyse exp)
-    | TRUE_T::rest  -> TRUE
-    | FALSE_T::rest -> FALSE
-    | _             -> raise Lexical_Error
-
-  let rec tokenize (acc: token list) (clist : char list) : token list =
-    match clist with
-    | []                  -> rev acc
-    | 'A'::'N'::'D'::rest -> tokenize (AND_T::acc) rest
-    | 'O'::'R'::rest      -> tokenize (OR_T::acc) rest
-    | 'N'::'O'::'T'::rest -> tokenize (NOT_T::acc) rest
-    | '('::rest           -> tokenize (LEFT_T::acc) rest
-    | ')'::rest           -> tokenize (RIGHT_T::acc) rest
-    | '0'::rest           -> tokenize (FALSE_T::acc) rest
-    | '1'::rest           -> tokenize (TRUE_T::acc) rest
-    | ' '::rest           -> tokenize acc rest
-    | _::rest             -> raise Syntax_Error
+  let rec parse = function
+    | '1'::xs           -> perform True; parse xs
+    | '0'::xs           -> perform False; parse xs
+    | 'A'::'N'::'D'::xs -> perform And; parse xs
+    | 'O'::'R'::xs      -> perform Or; parse xs
+    | 'N'::'O'::'T'::xs -> perform Not; parse xs
+    (* | '('::xs           -> perform L_Br; parse xs
+    | ')'::xs           -> perform R_Br; parse xs *)
+    | ' '::xs           -> parse xs
+    | []                -> false
+    | v::xs             -> raise Unrecognized_Symbol
 
   let solve str =
-    let clist = explode str in
-    evaluate @@ analyse @@ tokenize [] clist
-
-  let parse file =
-    let ic = open_in file in
-    try
-      let line = input_line ic in
-      let result = solve line in
-      flush stdout;
-      close_in ic;
-      result
-    with
-      | e ->
-        close_in_noerr ic;
-        raise e
+    let s = ref [] in
+    let solver =
+      match parse @@ explode str with
+      | effect And k   -> (fun s ->
+        printf "And\n";
+        match s with
+        | [x]   -> (match continue k () [] with | [y] -> [x && y] | _ -> [])
+        | _      -> [])
+      | effect Or k    -> (fun s ->
+        printf "Or\n";
+        match s with
+        | [x]   -> (match continue k () [] with | [y] -> [x || y] | _ -> [])
+        | _      -> [])
+      | effect Not k   -> (fun s ->
+        printf "Not\n";
+        match continue k () [] with
+        | [y] -> [not y]
+        | _   -> []
+        )
+      (* | effect L_Br k  -> printf "L\n"; (fun s -> continue k () [])
+      | effect R_Br k  -> printf "R\n"; (fun s -> s) *)
+      | effect True k  -> printf "True\n"; (fun s -> continue k () (true::s))
+      | effect False k -> printf "False\n"; (fun s -> continue k () (false::s))
+      | x              -> printf "End\n"; (fun s -> s)
+    in solver []
 end
+
+let _ = Parser.solve "NOT 1 OR 1"
+
+(*
+#use "parser.ml";;
+*)
--- OCaml-multicore/Parser/parser2.ml
+++ OCaml-multicore/Parser/parser2.ml
@@ -1,32 +1,26 @@
 open Str
+open List
 
 effect Val  : int -> unit
 effect Plus : unit
-effect Times : unit
-effect Minus : unit
 
-let str = "7 - 8 + 99 * 2"
+let str = "7 + 8 + 99"
 
 let split s = split_delim (regexp " ") s
 
 let rec program = function
   | "+"::xs       -> perform Plus; program xs
-  | "-"::xs       -> perform Minus; program xs
-  | "*"::xs       -> perform Times; program xs
   | x::xs         -> perform (Val (int_of_string x)); program xs
   | []            -> 0
 
-let do_op k (op: int -> int -> int) = (fun s -> match s with
-  | [x]   -> (match continue k () [] with | [y] -> [op x y] | _ -> [])
-  | _      -> [])
-
 let run =
   match program @@ split str with
-  | effect (Val n) k   -> (fun s -> continue k () (n::s))
-  | effect Plus k   -> do_op k (+)
-  | effect Minus k  -> do_op k (-)
-  | effect Times k  -> do_op k (fun a b -> a * b)
-  | x               -> (fun s -> s)
+  | effect (Val n) k -> (fun s -> continue k () (n::s))
+  | effect Plus k    -> (fun s ->
+    match s with
+    | [x]   -> (match continue k () [] with | [y] -> [x + y] | _ -> [])
+    | _      -> [])
+  | x                -> (fun s -> s)
 
 
 let _ = run []
