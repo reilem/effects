fc89026cba6f00187f648789564da7783281044a
BUSISOF <it.lucasengels@gmail.com>
2018-04-28 21:00:59 +0200

    Added StressTest

---
+++ Scala-effekt/Effekt/src/main/scala/StressTest.scala
@@ -0,0 +1,25 @@
+import effekt._
+
+trait Stress extends Eff {
+  def check(): Op[Boolean]
+}
+
+object StressTest {
+
+  def check()(implicit u: Use[Stress]): Control[Boolean] = use(u) { u.handler.check() }
+
+  def test()(implicit state: Use[Stress]): Control[Unit] = {
+    for {
+      check <- check()
+    } yield if (check) test() else ()
+  }
+
+  def stressHandler = new Handler.Stateful[Unit, Unit, Int] with Stress {
+    override def check(): Int => (Boolean => Int => Control[Unit]) => Control[Unit] =
+      state => resume => resume(state > 0)(state - 1)
+    override def unit: Unit => Unit = identity
+  }
+
+  def run(upperBound: Int): Unit = stressHandler(upperBound) { implicit h => test() }.run()
+}
+
