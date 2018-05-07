1dc911f4673739c7eea1b6c5e76c9df83b04f752
Reinert <reilemx@gmail.com>
2018-05-07 18:05:55 +0200

    Basic fringe implementation

---
+++ Scala-effekt/Effekt/src/main/scala/Fringe.scala
@@ -0,0 +1,72 @@
+import effekt._
+
+import scala.util.Random
+
+trait Waiter extends Eff {
+  def wait(value: Int): Op[Unit]
+}
+
+sealed trait Tree
+case class Leaf(value: Int) extends Tree
+case class Node(left: Tree, right: Tree) extends Tree
+
+object Fringe {
+
+  sealed trait State
+  case class Done() extends Fringe.State
+  case class Waiting(value: Int, continuation: Unit => Unit => Control[Fringe.State]) extends Fringe.State
+
+  def wait(value: Int)(implicit u: Use[Waiter]): Control[Unit] = use(u) { u.handler.wait(value) }
+
+  def step(t: Tree): Control[Fringe.State] = {
+    def walk(tree: Tree)(implicit handler: Use[Waiter]): Control[Unit] = {
+      tree match {
+        case Leaf(v) => for {
+          res <- wait(v)
+        } yield res
+        case Node(l, r) => walk(l); walk(r)
+      }
+    }
+    def fringeHandler = new Handler.Basic[Unit, Fringe.State] with Waiter {§
+      override def wait(value: Int): Unit => (Unit => Unit => Control[Fringe.State]) => Control[Fringe.State] =
+        _ => resume => pure(Waiting(value, resume))
+      override def unit: Unit => Fringe.State = _ => Done()
+    }
+    pure(fringeHandler { implicit h => walk(t) }.run())
+  }
+
+
+  def fringe(t1: Tree, t2: Tree): Control[Boolean] = {
+    def stepper(t: Tree): Unit => Control[Fringe.State] = _ => step(t)
+    def resume(k: Unit => Unit => Control[Fringe.State]): Unit => Control[Fringe.State] = k()
+    def solve(l: Unit => Control[Fringe.State], r: Unit => Control[Fringe.State]): Control[Boolean] = for {
+      left <- l()
+      right <- r()
+      result <- (left, right) match {
+        case (Waiting(v1, k1), Waiting(v2, k2)) => if (v1 == v2) solve(resume(k1), resume(k2)) else false
+        case (Done(), Done()) => true
+        case _ => false
+      }
+    } yield result
+    solve(stepper(t1), stepper(t2))
+  }
+
+
+  def generate(i: Int): Tree = {
+    Leaf(4)
+  }
+
+  def run(t1: Tree, t2: Tree): Control[Boolean] = fringe(t1, t2)
+
+  def main(args: Array[String]): Unit = {
+    if (args.length >= 1) {
+      val n = args(0).toInt
+      val t = generate(n)
+      val now = System.currentTimeMillis()
+      run(t, t)
+      val time = System.currentTimeMillis() - now
+      println(time)
+    }
+  }
+}
+
